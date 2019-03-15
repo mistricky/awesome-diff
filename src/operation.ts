18b294aaa257645497f66a27d1638df327f41bb0
HaoDaWang <1306036576@qq.com>
2019-03-15 16:16:13 +0800

    [Update] add method of operations

--- src/operation.ts
+++ src/operation.ts
@@ -1,12 +1,12 @@
 
-export type OPERATIONS = 'move' | 'replace' | 'add' | 'remove'
+export type OPERATIONS = 'move' | 'add' | 'remove'
 
 export interface MovePayload {
   originIndex:number;
   targetIndex:number
 }
 
-export interface ReplaceAddPayload<T> {
+export interface AddPayload<T> {
   targetIndex:number,
   target:T
 }
@@ -15,7 +15,26 @@ export interface RemovePayload {
   targetIndex:number
 }
 
-export interface Operation<T extends OPERATIONS = OPERATIONS, ReplaceTarget = unknown> {
+export interface Operation<T extends OPERATIONS = OPERATIONS, AddTarget = unknown> {
   name:T;
-  payload:T extends 'move' ? MovePayload : T extends 'replace' | 'add' ? ReplaceAddPayload<ReplaceTarget> : RemovePayload
+  payload:T extends 'move' ? MovePayload : T extends 'replace' | 'add' ? AddPayload<AddTarget> : RemovePayload
+}
+
+function duplicatePush(operations:Operation<OPERATIONS>[], operation:Operation){
+  let duplicate = operations.slice()
+  duplicate.push(operation)
+
+  return duplicate
+}
+
+export function add<T>(operations:Operation<OPERATIONS, T>[], targetIndex:number, target:T){
+  return duplicatePush(operations, {name:'add', payload:{targetIndex, target}})
+}
+
+export function remove(operations:Operation<OPERATIONS>[], targetIndex:number){
+  return duplicatePush(operations, {name:'remove', payload:{targetIndex}})
+}
+
+export function move(operations:Operation<OPERATIONS>[], originIndex:number, targetIndex:number){
+  return duplicatePush(operations, {name:'remove', payload:{targetIndex, originIndex}})
 }
