dacd082fe3b2398aff008c1299a5e2ce4f4af097
HaoDaWang <1306036576@qq.com>
2019-03-16 00:09:47 +0800

    [Add] filterUndefined

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { Operation, MovePayload, RemovePayload, AddPayload } from "./operation";
+import { VdomNodeLayer } from "./diff";
 
 export type Payloads<T> = MovePayload | RemovePayload | AddPayload<T>
 
@@ -14,11 +15,34 @@ export function groupByOperation(originArr:Operation[]):Group<unknown> {
   }, {})
 }
 
+export function filterUndefined<T>(layer:T[]):T[]{
+  return layer.filter((node, index) => (index === 0 && node) || node)
+}
+
 // insert method of array
-export function insert<T>(arr:T[],index:number, item:T[] ):T[]{
+export function insert<T>(arr:T[], index:number, ...item:T[]):T[]{
   let duplicate = arr.slice()
 
   duplicate.splice(index + 1, 0, ...item)
 
   return duplicate
+}
+
+export function move<T>(arr:T[], originIndex:number, targetIndex:number){
+  let duplicate = arr.slice()
+
+  duplicate[originIndex] = undefined
+  duplicate = insert(duplicate, targetIndex, arr[originIndex])
+
+  return filterUndefined(duplicate)
+}
+
+export function remove<T>(arr:T[], ...indexes: number[]) {
+  let duplicate = arr.slice()
+
+  for(let index of indexes){
+    duplicate[index] = undefined
+  }
+
+  return filterUndefined(duplicate)
 }
